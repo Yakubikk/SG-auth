f78f30079756eb4c1db2a1e0601801f1715139a5
Kirill <kavrusevich2002@gmail.com>
2025-04-13 23:16:33 +0300

    change connection string

--- back-end/back-end/Program.cs
+++ back-end/back-end/Program.cs
@@ -54,6 +54,12 @@ if (builder.Environment.IsDevelopment())
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate(); 
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
